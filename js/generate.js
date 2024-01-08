6a94d3c0c7e77d37b88fcd98ea78d0ebea273f1a
rafiulhasan1 <rafiulhasan001@gmail.com>
2024-01-08 17:42:25 +0600

    pin submit part added

--- js/generate.js
+++ js/generate.js
@@ -18,3 +18,30 @@ document.getElementById('generate-pin').addEventListener('click',function(){
     pinFrom.value = generate;
     //pin-from
 })
+
+const numberField = document.getElementById('numbers-field');
+
+
+document.getElementById('calculator').addEventListener('click',function(event){
+    const calculatorNumber = event.target.innerText;
+    const stringCalculatorNumber = numberField.value;
+    const totalNumber = stringCalculatorNumber+calculatorNumber;
+    if(isNaN(calculatorNumber)){
+        if(calculatorNumber === 'C'){
+            numberField.value = '';
+        }
+        else if(calculatorNumber === '<'){
+            const digit = stringCalculatorNumber.split('');
+            digit.pop();
+            const remainingDigits = digit.join('');
+            numberField.value = remainingDigits;
+        }
+        else if(calculatorNumber === 'Submit'){
+            
+        }
+    }
+    else{
+        numberField.value = totalNumber;
+    }
+    
+})
